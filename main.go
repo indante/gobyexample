5b114b640a1d29bf5c4a9e0c1e278507959b3bf9
indante <koreanhoseon@gmail.com>
2022-05-20 17:56:00 +0900

    Add an example

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "gobyexample/switchexp"
+
+func main() {
+	//helloworld.HelloWorld()
+	//values.PrintValues()
+	//variables.PrintVariables()
+	//constants.PrintConstants()
+	//loop.Loop()
+	//ifelse.PrintValuesConditionally()
+	switchexp.Switch()
+}
